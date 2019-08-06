4938f73210a05d763036b054ec01fe7f450480e2
HaiNH <hoanghai18497@gmail.com>
2019-08-07 00:24:47 +0700

    init2

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -1,44 +1,45 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-19">
-                <h1 class="title">Books</h1>
-                    <hr><br><br>
-                    <button type="button" class="btn btn-success btn-sm">Add Book</button>
-                    <br><br>
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th scope="col">Title</th>
-                                <th scope="col">Author</th>
-                                <th scope="col">Read?</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="(book, index) in books" :key="index">
-                                <td>{{ book.title }}</td>
-                                <td>{{ book.author }}</td>
-                                <td>
-                                    <span v-if="book.read">Yes</span>
-                                    <span v-else>No</span>
-                                </td>
-                                <td>
-                                    <div class="btn-group" role="group">
-                                        <button type="button" class="btn btn-warning btn-sm">Update</button>
-                                        <button type="button" class="btn btn-danger btn-sm">Delete</button>
-                                    </div>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-10">
+        <h1 class="title">Books</h1>
+        <hr><br><br>
+        <button type="button" class="btn btn-success btn-sm">Add Book</button>
+        <br><br>
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Author</th>
+              <th scope="col">Read?</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(book, index) in books" :key="index">
+              <td>{{ book.title }}</td>
+              <td>{{ book.author }}</td>
+              <td>
+                <span v-if="book.read">Yes</span>
+                <span v-else>No</span>
+              </td>
+              <td>
+                <div class="btn-group" role="group">
+                  <button type="button" class="btn btn-warning btn-sm">Update</button>
+                  <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+          </table>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 import axios from 'axios';
+
 export default {
   data() {
     return {
@@ -63,4 +64,3 @@ export default {
   },
 };
 </script>
-
--- client/src/components/Ping.vue
+++ client/src/components/Ping.vue
@@ -16,19 +16,19 @@ export default {
     };
   },
   methods: {
-      getMessage() {
-          const path = 'http://localhost:5000/ping';
-          axios.get(path)
-            .then((res) => {
-                this.msg = res.data;
-            }) 
-            .catch((error) => {
-                console.error(error);
-            }); 
-      },
+    getMessage() {
+      const path = 'http://localhost:5000/ping';
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
   },
   created() {
-      this.getMessage();
+    this.getMessage();
   },
 };
 </script>
